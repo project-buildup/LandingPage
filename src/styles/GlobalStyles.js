67b1bde4cb2c186106340017003868440aade505
Sebyeok <tngnu02@gmail.com>
2023-05-11 19:52:08 +0900

    Fix: Loading Latency

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,46 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
-import GmarketSans from '../assets/fonts//GmarketSans.woff2';
-import PretendardExtraBold from '../assets/fonts//Pretendard-ExtraBold.woff2';
-import PretendardBold from '../assets/fonts//Pretendard-Bold.woff2';
-import PretendardSemiBold from '../assets/fonts//Pretendard-SemiBold.woff2';
-import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
+// import GmarketSans from '../assets/fonts//GmarketSans.woff2';
+// import PretendardExtraBold from '../assets/fonts//Pretendard-ExtraBold.woff2';
+// import PretendardBold from '../assets/fonts//Pretendard-Bold.woff2';
+// import PretendardSemiBold from '../assets/fonts//Pretendard-SemiBold.woff2';
+// import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
 
 export default createGlobalStyle`
-  @font-face {
-    font-family: "GmarketSans";
-    src: local("GmarketSansBold"), local("GmarketSans-Bold"), local("Gmarket Sans Bold"),url(${GmarketSans}) format("woff2");
-    font-weight: 700;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: "Pretendard";
-    src: local("Pretendard-ExtraBold"), url(${PretendardExtraBold}) format("woff2");
-    font-weight: 800;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: "Pretendard";
-    src: local("Pretendard-Bold"), url(${PretendardBold}) format("woff2");
-    font-weight: 700;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: "Pretendard";
-    src: local("Pretendard-SemiBoldd"), url(${PretendardSemiBold}) format("woff2");
-    font-weight: 600;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: "Pretendard";
-    src: local("Pretendard-Regular"), url(${PretendardRegular}) format("woff2");
-    font-weight: 400;
-    font-style: normal;
-  }
-
   @keyframes fadeInObject {
     0% {
       opacity: 0;
